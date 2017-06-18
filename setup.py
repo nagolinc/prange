3693547fa2e143dfcedce4360b7ca94787537584
logan <nagolinc@gmail.com>
2017-06-17 21:20:01 -0400

    trying to get data_files right

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@
 from distutils.core import setup
 import glob
 
-os.chdir("src/prange")
-data_files=glob.glob('data/*.txt')
+#os.chdir("src/prange")
+#data_files=glob.glob('data/*.txt')
 #data_files=glob.glob('src/prange/data/*.txt')
 
 
@@ -20,7 +20,8 @@ setup(name='prange',
       packages=['prange'],
       package_dir={'prange': 'src/prange'},
       #data_files=[('data',data_files)]
-      package_data={  'prange': data_files }
+      #package_data={  'prange': data_files }
+      package_data={  'prange': ['data/commonNouns.txt','data/adventure.txt'}
       #data_files=[('data', ['src/prange/data/commonNouns.txt', 'src/prange/data/adventure.txt'])]
       
      )
