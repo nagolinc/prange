c58cb98310a2d15d10e40e29a91b9fca29ad59f9
logan <nagolinc@gmail.com>
2017-06-17 21:22:19 -0400

    think I finally got it

--- setup.py
+++ setup.py
@@ -3,13 +3,6 @@
 from distutils.core import setup
 import glob
 
-#os.chdir("src/prange")
-#data_files=glob.glob('data/*.txt')
-#data_files=glob.glob('src/prange/data/*.txt')
-
-
-#print data_files
-
 setup(name='prange',
       version='0.1',
       description='A Complex Python Progress Meter',
@@ -19,10 +12,6 @@ setup(name='prange',
       
       packages=['prange'],
       package_dir={'prange': 'src/prange'},
-      #data_files=[('data',data_files)]
-      #package_data={  'prange': data_files }
-      #package_data={  'prange': ['data/commonNouns.txt','data/adventure.txt']}
       package_data={  'prange': ['data/*.txt']}
-      #data_files=[('data', ['src/prange/data/commonNouns.txt', 'src/prange/data/adventure.txt'])]
       
      )
